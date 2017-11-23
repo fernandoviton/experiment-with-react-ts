d185333d49c401782b20352d2fb3a8460322db8c
Fernando Viton <fviton@microsoft.com>
2017-11-22 20:31:46 -0800

    Move sample.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import * as React from 'react';
 import './App.css';
 import JSONTree from 'react-json-tree';
-import fs from 'fs';
+import { SampleJson } from './sampleJson';
 
 const logo = require('./logo.svg');
 
-const json = {
-    array: [1, 2, 3],
-    bool: true,
-    object: {
-      foo: 'bar'
-    }};
-
 class App extends React.Component {
-  fs.readFile('sample.json', (err, data) => {
-    console.log(data);
-  });
-
   render() {
     return (
       <div className="App">
@@ -24,7 +13,7 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <JSONTree data={json}/>
+        <JSONTree data={SampleJson}/>
       </div>
     );
   }
